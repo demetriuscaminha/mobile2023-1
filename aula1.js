47b13b768613f32f1bca5971f5c582645e2e2650
201201075157 <201201075157@LABTI14.local>
2023-03-09 21:04:38 -0300

    Update aula1.js

---
+++ aula1.js
@@ -0,0 +1,10 @@
+var nome1 = "JOÃO"; //string
+nome1 = "MATEUS"; //string
+nome1 = 85996710231; //number
+nome1 = true; // boolean
+nome1 = ["Pedro", 123, "Felipe", {nome: "Matheus"}, 1.765]; //array
+
+let nome2 = "JOSÉ"; //string
+nome2 = "FELIPE"; //string
+
+cont salario = 1300 //number

d11f8adbf0be30c4603b011c94d757ed1eed6775
DEMETRIUS CAMINHA DE MENEZES PADILHA <201201075157@alunos.estacio.br>
2023-03-30 20:53:16 -0300

    Coponentes Básicos React

--- appzinho/App.js
+++ appzinho/App.js
@@ -1,17 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, TextInput, Button, Alert } from 'react-native';
 
 
 export default function App() {
+  const text = 'Campo Input';
   return (
     <View style={styles.container}>
       <Image
         style={{width: 50, height: 50}}
         source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
-      />
+        />
       <Text style={styles.appTitle}>Hello World!</Text>
       <Text style={styles.typeTextPrimary}>Aluno: Demetrius Caminha</Text>
       <Text>Sala 102</Text>
+      <Text style={styles.appTitle}>Efetuar Login</Text>
+      <SafeAreaView>
+        <TextInput
+          style={styles.inputField}
+          value={text}
+        />
+        <TextInput
+          style={styles.inputField}
+          secureTextEntry={true}
+        />
+        <Button
+          title="Enviar Alerta"
+          onPress={() => Alert.alert('Exemplo de alerta!')}
+        />
+      </SafeAreaView>
       <StatusBar style="auto" />
     </View>
   );
@@ -38,5 +54,13 @@ const styles = StyleSheet.create({
     margin: 20,
     borderRadius: 10,
     transform: 'rotate(180deg)'
+  },
+  inputField: {
+    borderColor: 'gray',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    padding: 8,
+    borderRadius: 10,
+    marginTop: 15
   }
 });

4bc79e23687e10a39ff5e5c12f20587fde609298
DEMETRIUS CAMINHA DE MENEZES PADILHA <201201075157@alunos.estacio.br>
2023-03-30 20:23:27 -0300

    Coponentes Básicos React

--- appzinho/App.js
+++ appzinho/App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        style={{width: 50, height: 50}}
+        source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
+      />
+      <Text style={styles.appTitle}>Hello World!</Text>
+      <Text style={styles.typeTextPrimary}>Aluno: Demetrius Caminha</Text>
+      <Text>Sala 102</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +24,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  appTitle: {
+    fontSize: 28,
+    color: 'red',
+    textAlign: 'center'
+  },
+  typeTextPrimary: {
+    textTransform: 'uppercase',
+    color: 'white',
+    backgroundColor:'#000',
+    padding: 10,
+    fontWeight: 'bold',
+    margin: 20,
+    borderRadius: 10,
+    transform: 'rotate(180deg)'
+  }
 });
